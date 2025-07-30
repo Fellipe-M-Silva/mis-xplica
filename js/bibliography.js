b31bc7e654dea633b25a30b463cc6a5701632a17
Fellipe Silva <fmayan999@gmail.com>
2025-07-30 18:23:20 -0300

    feat: ajuste de layout e card explicativo do raio-x

---
+++ js/bibliography.js
@@ -0,0 +1,7 @@
+document.addEventListener("DOMContentLoaded", () => {
+  // Função para rolar a página até o elemento especificado
+  const scrollToElement = (element) => {
+    element.scrollIntoView({ behavior: "smooth", block: "start" });
+  };
+  // Seleciona os elementos necessários
+});
